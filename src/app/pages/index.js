f0c2bc0aea1dd27ddaf77a28e069d53ee60a1a0f
IvanKharkivskyi1 <ikcomua@gmail.com>
2024-11-26 22:37:21 +0200

    asdf

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -1,11 +1,12 @@
 // pages/index.js
-import SportsList from '../components/SportsList';
+// import SportsList from '../components/SportsList';
 
 export default function Home() {
   return (
     <div>
       <h1>Hello World from Next.js and Redux</h1>
-      <SportsList />
+      {/* <SportsList /> */}
+      <h2>test</h2>
     </div>
   );
 }
